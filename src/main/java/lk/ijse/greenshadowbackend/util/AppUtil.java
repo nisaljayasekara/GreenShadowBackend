bebfcbc1eb1b51ab8e4bb7b09325d3b5a78ab23a
nisaljayasekara <j.nmjayasekara@gmail.com>
2024-11-27 14:47:01 +0530

    vehicle code generated

--- src/main/java/lk/ijse/greenshadowbackend/util/AppUtil.java
+++ src/main/java/lk/ijse/greenshadowbackend/util/AppUtil.java
@@ -1,4 +1,11 @@
 package lk.ijse.greenshadowbackend.util;
 
 public class AppUtil {
+
+    private static int vehicleCounter = 0;
+
+    public static synchronized String createVehicleCode(){
+        vehicleCounter++;
+        return String.format("V%04d", vehicleCounter);
+    }
 }
--- src/main/java/lk/ijse/greenshadowbackend/util/Mapping.java
+++ src/main/java/lk/ijse/greenshadowbackend/util/Mapping.java
@@ -1,11 +1,28 @@
 package lk.ijse.greenshadowbackend.util;
 
+import lk.ijse.greenshadowbackend.dto.impl.VehicleDTO;
+import lk.ijse.greenshadowbackend.entity.VehicleEntity;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class Mapping {
     @Autowired
     private ModelMapper modelMapper;
+    public Mapping(){this.modelMapper = new ModelMapper();}
+
+    //VehicleEntity and DTO
+    public VehicleDTO convertToVehicleDTO(VehicleEntity vehicle){
+        return modelMapper.map(vehicle, VehicleDTO.class);
+    }
+    public VehicleEntity convertToVehicleEntity(VehicleDTO dto){
+        return modelMapper.map(dto, VehicleEntity.class);
+    }
+    public List<VehicleDTO> convertVehicleToDTOList(List<VehicleEntity> vehicles){
+        return modelMapper.map(vehicles, new TypeToken<List<VehicleDTO>>(){}.getType());
+    }
 }
