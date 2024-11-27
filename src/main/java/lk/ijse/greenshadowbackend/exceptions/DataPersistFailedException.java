8c8c0cc002f672592e1f35348aaa225f073401e7
nisaljayasekara <j.nmjayasekara@gmail.com>
2024-11-27 14:47:29 +0530

    DataPersistFailedException class applied

---
+++ src/main/java/lk/ijse/greenshadowbackend/exceptions/DataPersistFailedException.java
@@ -0,0 +1,7 @@
+package lk.ijse.greenshadowbackend.exceptions;
+
+public class DataPersistFailedException extends RuntimeException{
+    public DataPersistFailedException(){}
+    public DataPersistFailedException(String message){}
+    public DataPersistFailedException(String message, Throwable cause){}
+}
