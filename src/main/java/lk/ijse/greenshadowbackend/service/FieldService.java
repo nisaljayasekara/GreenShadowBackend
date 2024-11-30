1e02a6a434b1ebce3d29e239860fcb45c0143f7b
nisaljayasekara <j.nmjayasekara@gmail.com>
2024-11-30 19:00:26 +0530

    Field Search completed

--- src/main/java/lk/ijse/greenshadowbackend/service/FieldService.java
+++ src/main/java/lk/ijse/greenshadowbackend/service/FieldService.java
@@ -1,5 +1,6 @@
 package lk.ijse.greenshadowbackend.service;
 
+import lk.ijse.greenshadowbackend.customObj.FieldResponse;
 import lk.ijse.greenshadowbackend.dto.impl.FieldDTO;
 
 import java.util.List;
@@ -8,6 +9,7 @@ public interface FieldService {
     void saveField(FieldDTO fieldDTO);
 
     List<FieldDTO> getAllFields();
+    FieldResponse getSelectedField(String fieldCode);
 
 
 }
